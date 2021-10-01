acb33c08cb737372addbd8117e4f4656b3ac2f6c
zill4 <justcrisp@outlook.com>
2021-09-30 18:51:34 -0700

    channging dist

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production'    ? '/warlok-scheduler/'    : '/'
-}
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/warlok-scheduler/'
+    : '/'
+  }
